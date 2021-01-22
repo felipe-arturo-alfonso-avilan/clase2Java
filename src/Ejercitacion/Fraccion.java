22b1cca828a99bffcdc6c39e712fe5adcf38afef
Macarena Pecha <macarena.pecha@mercadolibre.com>
2021-01-22 15:35:02 -0300

    ej4 fix

--- src/Ejercitacion/Fraccion.java
+++ src/Ejercitacion/Fraccion.java
@@ -1,122 +1,142 @@
 package Ejercitacion;
 
 public class Fraccion {
-    private double valor;
+    private int numerador;
+    private int denominador;
 
     public Fraccion(){
 
     }
 
-    public double getValor() {
-        return valor;
+    public int getNumerador() {
+        return numerador;
     }
 
-    public void setValor(double valor) {
-        this.valor = valor;
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public double resultado(){
+        return this.getNumerador()/(double)this.getDenominador();
     }
 
     public static double sumar(int a, int b){
         return a+b;
     }
 
-    public static double sumar(double a, int b){
-        return a+b;
+    public static double sumar(Fraccion f, int b){
+        return f.resultado()+b;
     }
 
-    public static double sumar(int a, double b){
-        return a+b;
+    public static double sumar(int a, Fraccion f){
+        return a+f.resultado();
     }
 
-    public static double sumar(double a, double b){
-        return a+b;
+    public static double sumar(Fraccion f1, Fraccion f2){
+        return f1.resultado()+ f2.resultado();
     }
 
-    public double sumar(double a){
+/*    public double sumar(double a){ // creo que esto no va
         return a + this.getValor();
-    }
+    }*/
 
     public static double restar(int a, int b){
         return a-b;
     }
 
-    public static double restar(double a, int b){
-        return a-b;
+    public static double restar(Fraccion f, int b){
+        return f.resultado()-b;
     }
 
-    public static double restar(int a, double b){
-        return a-b;
+    public static double restar(int a, Fraccion f){
+        return a-f.resultado();
     }
 
-    public static double restar(double a, double b){
-        return a-b;
+    public static double restar(Fraccion f1, Fraccion f2){
+        return f1.resultado()- f2.resultado();
     }
 
-    public double restar(double a){
+/*    public double restar(double a){ // creo que esto no va
         return a - this.getValor();
-    }
+    }*/
 
     public static double dividir(int a, int b){
         return a/(double)b;
     }
 
-    public static double dividir(double a, int b){
-        return a/b;
+    public static double dividir(Fraccion f, int b){
+        return f.resultado()/b;
     }
 
-    public static double dividir(int a, double b){
-        return a/b;
+    public static double dividir(int a, Fraccion f){
+        return a/f.resultado();
     }
 
-    public static double dividir(double a, double b){
-        return a/b;
+    public static double dividir(Fraccion f1, Fraccion f2){
+        return f1.resultado()/f2.resultado();
     }
 
-    public double dividir(double a){
+/*
+    public double dividir(double a){ // creo que esto no va
         return a / this.getValor();
     }
+*/
 
     public static double multiplicar(int a, int b){
         return a*b;
     }
 
-    public static double multiplicar(double a, int b){
-        return a*b;
+    public static double multiplicar(Fraccion f, int b){
+        return f.resultado()*b;
     }
 
-    public static double multiplicar(int a, double b){
-        return a*b;
+    public static double multiplicar(int a, Fraccion f){
+        return a*f.resultado();
     }
 
-    public static double multiplicar(double a, double b){
-        return a*b;
+    public static double multiplicar(Fraccion f1, Fraccion f2){
+        return f1.resultado()*f2.resultado();
     }
 
-    public double multiplicar(double a){
+/*    public double multiplicar(double a){ // creo que esto no va
         return a * this.getValor();
-    }
+    }*/
 
     public static void main(String[] args) {
-        Fraccion f = new Fraccion();
-        f.setValor(3);
+        Fraccion f1 = new Fraccion();
+        Fraccion f2 = new Fraccion();
+        f1.setNumerador(5);
+        f1.setDenominador(2);
+        f2.setNumerador(7);
+        f2.setDenominador(3);
+
         System.out.println(Fraccion.sumar(1,2));
-        System.out.println(Fraccion.sumar(1.5,2));
-        System.out.println(Fraccion.sumar(1.5,2.6));
-        System.out.println(Fraccion.sumar(1,2.7));
+        System.out.println(Fraccion.sumar(f1,2));
+        System.out.println(Fraccion.sumar(f1,f2));
+        System.out.println(Fraccion.sumar(1,f2));
 
         System.out.println(Fraccion.restar(1,2));
-        System.out.println(Fraccion.restar(1.5,2));
-        System.out.println(Fraccion.restar(1.5,2.6));
-        System.out.println(Fraccion.restar(1,2.7));
+        System.out.println(Fraccion.restar(f1,2));
+        System.out.println(Fraccion.restar(f1,f2));
+        System.out.println(Fraccion.restar(1,f2));
 
         System.out.println(Fraccion.multiplicar(1,2));
-        System.out.println(Fraccion.multiplicar(1.5,2));
-        System.out.println(Fraccion.multiplicar(1.5,2.6));
-        System.out.println(Fraccion.multiplicar(1,2.7));
+        System.out.println(Fraccion.multiplicar(f1,2));
+        System.out.println(Fraccion.multiplicar(f1,f2));
+        System.out.println(Fraccion.multiplicar(1,f2));
 
         System.out.println(Fraccion.dividir(1,2));
-        System.out.println(Fraccion.dividir(1.5,2));
-        System.out.println(Fraccion.dividir(1.5,2.6));
-        System.out.println(Fraccion.dividir(1,2.7));
+        System.out.println(Fraccion.dividir(f1,2));
+        System.out.println(Fraccion.dividir(f1,f2));
+        System.out.println(Fraccion.dividir(1,f2));
 
     }
 }
